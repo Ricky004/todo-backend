879565ee91133c3821f6cc14968ee60448765120
Ricky004 <tanaydam000@gmail.com>
2023-09-24 21:01:04 +0530

    Revert "add text-- maxLength and minLength in TodoModel"
    This reverts commit c505b6f784215a0e34d40a86fe09637f49a80213.

--- models/TodoModel.js
+++ models/TodoModel.js
@@ -4,8 +4,6 @@ const todoSchema = new mongoose.Schema({
     text: {
         type: String,
         require: true,
-        maxLength: 30,
-        minLength: 1
     },
     isDone: {
        type: Boolean,
